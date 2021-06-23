189535d8abd6792cafa2ed28013d075eb5b01fce
Kurt M. Ma. Coll <kurt_ma.coll@live.com>
2021-06-23 14:45:47 +0800

    Add test to enumerate all row-col pairs of 5sqr

--- __tests__/Parts_test.bs.js
+++ __tests__/Parts_test.bs.js
@@ -3,6 +3,7 @@
 
 var Cell = require("../src/Cell.bs.js");
 var Jest = require("@glennsl/bs-jest/src/jest.bs.js");
+var Belt_List = require("rescript/lib/js/belt_List.js");
 
 Jest.describe("Two basic parts belong to the same square ", (function (param) {
         return Jest.test("parts_of_same_square", (function (param) {
@@ -34,6 +35,173 @@ Jest.describe("Cell is valid", (function (param) {
                                           base: 5,
                                           index: 27
                                         })));
+                    }));
+      }));
+
+Jest.describe("All row-col pairs of 5-square", (function (param) {
+        return Jest.test("5-square_row-col_pairs", (function (param) {
+                      var allCells = Belt_List.map(Belt_List.makeBy(25, (function (i) {
+                                  return i + 1 | 0;
+                                })), (function (x) {
+                              return Cell.rowColumnPair({
+                                          base: 5,
+                                          index: x
+                                        });
+                            }));
+                      console.log(Belt_List.makeBy(25, (function (i) {
+                                  return i + 1 | 0;
+                                })));
+                      return Jest.Expect.toEqual({
+                                  hd: [
+                                    1,
+                                    1
+                                  ],
+                                  tl: {
+                                    hd: [
+                                      1,
+                                      2
+                                    ],
+                                    tl: {
+                                      hd: [
+                                        1,
+                                        3
+                                      ],
+                                      tl: {
+                                        hd: [
+                                          1,
+                                          4
+                                        ],
+                                        tl: {
+                                          hd: [
+                                            1,
+                                            5
+                                          ],
+                                          tl: {
+                                            hd: [
+                                              2,
+                                              1
+                                            ],
+                                            tl: {
+                                              hd: [
+                                                2,
+                                                2
+                                              ],
+                                              tl: {
+                                                hd: [
+                                                  2,
+                                                  3
+                                                ],
+                                                tl: {
+                                                  hd: [
+                                                    2,
+                                                    4
+                                                  ],
+                                                  tl: {
+                                                    hd: [
+                                                      2,
+                                                      5
+                                                    ],
+                                                    tl: {
+                                                      hd: [
+                                                        3,
+                                                        1
+                                                      ],
+                                                      tl: {
+                                                        hd: [
+                                                          3,
+                                                          2
+                                                        ],
+                                                        tl: {
+                                                          hd: [
+                                                            3,
+                                                            3
+                                                          ],
+                                                          tl: {
+                                                            hd: [
+                                                              3,
+                                                              4
+                                                            ],
+                                                            tl: {
+                                                              hd: [
+                                                                3,
+                                                                5
+                                                              ],
+                                                              tl: {
+                                                                hd: [
+                                                                  4,
+                                                                  1
+                                                                ],
+                                                                tl: {
+                                                                  hd: [
+                                                                    4,
+                                                                    2
+                                                                  ],
+                                                                  tl: {
+                                                                    hd: [
+                                                                      4,
+                                                                      3
+                                                                    ],
+                                                                    tl: {
+                                                                      hd: [
+                                                                        4,
+                                                                        4
+                                                                      ],
+                                                                      tl: {
+                                                                        hd: [
+                                                                          4,
+                                                                          5
+                                                                        ],
+                                                                        tl: {
+                                                                          hd: [
+                                                                            5,
+                                                                            1
+                                                                          ],
+                                                                          tl: {
+                                                                            hd: [
+                                                                              5,
+                                                                              2
+                                                                            ],
+                                                                            tl: {
+                                                                              hd: [
+                                                                                5,
+                                                                                3
+                                                                              ],
+                                                                              tl: {
+                                                                                hd: [
+                                                                                  5,
+                                                                                  4
+                                                                                ],
+                                                                                tl: {
+                                                                                  hd: [
+                                                                                    5,
+                                                                                    5
+                                                                                  ],
+                                                                                  tl: /* [] */0
+                                                                                }
+                                                                              }
+                                                                            }
+                                                                          }
+                                                                        }
+                                                                      }
+                                                                    }
+                                                                  }
+                                                                }
+                                                              }
+                                                            }
+                                                          }
+                                                        }
+                                                      }
+                                                    }
+                                                  }
+                                                }
+                                              }
+                                            }
+                                          }
+                                        }
+                                      }
+                                    }
+                                  }
+                                }, Jest.Expect.expect(allCells));
                     }));
       }));
 
--- __tests__/Parts_test.res
+++ __tests__/Parts_test.res
@@ -32,3 +32,25 @@ describe("Cell is valid", () => {
       expect(Cell.isValid(cell)) |> toBe(false)
     })
 })
+
+describe("All row-col pairs of 5-square", () => {
+  open Expect
+
+  test ("5-square_row-col_pairs", () => {
+      let base = 5
+      let allCells: list<(int,int)> = Belt.List.map(
+        Belt.List.makeBy(base*base, i => i+1),
+        (x) => {
+          let cell: Cell.part = {base: base, index: x}
+          Cell.rowColumnPair(cell)
+        })
+
+      expect(allCells) |> toEqual(list{
+        (1,1), (1,2), (1,3), (1,4), (1,5),
+        (2,1), (2,2), (2,3), (2,4), (2,5),
+        (3,1), (3,2), (3,3), (3,4), (3,5),
+        (4,1), (4,2), (4,3), (4,4), (4,5),
+        (5,1), (5,2), (5,3), (5,4), (5,5)
+      })
+    })
+})
